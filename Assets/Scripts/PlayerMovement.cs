533be8fd921847e6481bbcd518ba681dcad32962
GabrielNilsson <Gabriel.Nilsson@elev.ga.lbs.se>
2023-11-20 14:24:54 +0100

    Missade ett

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,76 +2,17 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public enum WeaponState
-{
-    Unarmed,
-    Hitscan,
-    Projectile,
-    Total
-}
 public class PlayerMovement : MonoBehaviour
 {
-    public List<Weapon> allHeldWeapons = new List<Weapon>();
+    public static Camera myCamera = null;
 
-    public Weapon currentWeapon = null;
-
-    [SerializeField] float speed = 12f;
-    [SerializeField] float gravity = -9.82f;
-    [SerializeField] Transform groundCheck;
-    [SerializeField] float groundDistance = 0.4f;
-    [SerializeField] LayerMask groundLayerMask;
-    [SerializeField] float jumpForce = 3f;
-    [SerializeField] float scrollWeaponSwapBreakpoint = 1f;
-
-    [SerializeField] CharacterController controller;
-
-    private Vector3 velocity;
-    private bool isGrounded;
-    private float mouseAxisDelta = 0f;
-    private WeaponState currentWeaponPointer = WeaponState.Unarmed;
-
-    private void Update()
+    public void Start()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundLayerMask);
-
-        if( isGrounded && velocity.y < 0 )
-        {
-            velocity.y = -2f;
-        }
-
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        mouseAxisDelta += Input.mouseScrollDelta.y;
-
-        Vector3 move = transform.right * x + transform.forward * z;
-
-        controller.Move(move * speed * Time.deltaTime);
-
-        if(Input.GetButtonDown("Jump") && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jumpForce * -2 * gravity);
-        }
-
-        velocity.y += gravity * Time.deltaTime;
-
-        controller.Move(velocity * Time.deltaTime);
-
-        if(Mathf.Abs(mouseAxisDelta) > scrollWeaponSwapBreakpoint)
-        {
-            
-        }
-        if(currentWeaponPointer == WeaponState.Unarmed)
-        {
-
-        }
+        PlayerMovement.myCamera = Camera.main;
     }
-
-    public void FireHeldWeapon()
+    public void Update()
     {
-        if(currentWeapon != null)
-        {
-            currentWeapon.Fire();
-        }
+
+
     }
 }
